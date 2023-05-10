4387b2910605af2c93c8d276c107f12cd360e67f
Krzysztof Wilgucki <wilguckikrzysztof0@gmail.com>
2023-05-10 20:14:13 +0200

    My first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function mainpage() {
+  window.open("index.html", "_self");
+}
+
+function aboutme() {
+  window.open("aboutme.html", "_self");
+}
+
+function supportme() {
+  window.open("supportme.html", "_self");
+}
+
+function music() {
+  window.open("music.html", "_self");
+}
